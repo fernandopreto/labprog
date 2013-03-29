3ac7be783df1f6a809fc442f47eadba2194930af
631220003 <631220003@Sala603-08.fspoaeduc.com.br>
2013-03-28 21:39:30 -0300

    trabalho 5

--- entregas/trabalho5/src/main/java/br/com/senacrs/alp/aulas/MinhaFila.java
+++ entregas/trabalho5/src/main/java/br/com/senacrs/alp/aulas/MinhaFila.java
@@ -1,12 +1,14 @@
-package br.com.senacrs.alp.aulas;
+/*package br.com.senacrs.alp.aulas;
 
-public class MinhaFila<T> implements Fila{//implements Fila
+public class MinhaFila<T> implements Fila<T>{//implements Fila
 	
 	Lista<T> lista = ListaFactory.getInstancia().criarLista();//instancia da lista
 	
 	@Override
 	public void insercao(Object valor) {
 		// TODO Auto-generated method stub
+		
+		lista.adicionarPosicao(lista.obterTamanho(), valor);
 		
 	}
 
@@ -39,6 +41,63 @@ public class MinhaFila<T> implements Fila{//implements Fila
 	public void esvaziar() {
 		// TODO Auto-generated method stub
 		
+	}
+
+}*/
+
+package br.com.senacrs.alp.aulas;
+
+public class MinhaFila<T> implements Fila<T> {
+
+	Lista<T> lista = ListaFactory.getInstancia().criarLista();
+
+	@Override
+	public void insercao(T valor) {
+
+		lista.adicionarPosicao(lista.obterTamanho(), valor);
+
+	}
+
+	@Override
+	public T obter() {
+		
+		return lista.obterPosicao(0);
+
+		//return null;
+
+	}
+
+	@Override
+	public T remocao() {
+
+		return lista.removerPosicao(0);
+
+	}
+
+	@Override
+	public boolean vazia() {
+		
+		boolean resultado = true;
+		
+		if(lista.obterTamanho()>0) {
+			resultado = false;
+		}
+		return resultado;
+
+	}
+
+	@Override
+	public int tamanho() {
+
+		return lista.obterTamanho();
+
+	}
+
+	@Override
+	public void esvaziar() {
+
+		lista.esvaziar();
+
 	}
 
 }
