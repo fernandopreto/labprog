dc3b9f4368bd68783085fd58078ffde1562aa8c1
LUCAS <LUCAS@192.168.2.3>
2013-04-15 23:55:45 -0300

    trabalho7

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/ComparatorFactory.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/ComparatorFactory.java
@@ -3,30 +3,30 @@ package br.com.senacrs.alp.aulas;
 import java.util.Comparator;
 
 public class ComparatorFactory {
-
+	
 	private static final ComparatorFactory instancia = new ComparatorFactory();
-
+	
 	private ComparatorFactory() {
 	}
-
+	
 	public Comparator<Integer> criarComparatorParesAntesDeImparesCrescente() {
-
+		
 		Comparator<Integer> resultado = null;
-
-		resultado = new MeuComparatorInt();
-
+		
+		resultado = new MeuComparatorINT();
+		
 		return resultado;
 	}
-
+	
 	public Comparator<String> criarComparatorOrdemAlfabeticaReversa() {
-
+		
 		Comparator<String> resultado = null;
-
-		resultado = new MeuComparator();
-
+		
+		resultado = new MeuComparatorSTRING();
+		
 		return resultado;
 	}
-
+	
 	public static ComparatorFactory getInstancia() {
 		return instancia;
 	}
