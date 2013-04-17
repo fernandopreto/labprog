86626fb1fb1d2670d88b6687ba936b25d07c261d
631220003 <631220003@Sala603-13.fspoaeduc.com.br>
2013-04-16 21:46:35 -0300

    trabalho 7

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorSTRING.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorSTRING.java
@@ -6,16 +6,41 @@ public class MeuComparatorSTRING implements Comparator<String> {
 
 	@Override
 	public int compare(String o1, String o2) {
-
+		
+		int resultado = 0;
+		
+		resultado = o2.compareTo(o1);
+		
+		return resultado;
+	}
+}/*		
+		
+		
+		
+		
 		if (o1 == null && o2 == null) {
+			
 			return 0;
+			
 		} else if (o1 != o2) {
+			
 			return -1;
+			
 		} else if (o1 == o2) {
+			
 			return 1;
+			
 		} else {
+			
 			return o2.compareTo(o1);
+			
 		}
 	}
 
 }
+*/
+
+
+
+
+
