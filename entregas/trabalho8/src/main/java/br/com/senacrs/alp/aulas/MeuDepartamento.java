a60e75632b56df2dfe8d5d876a69696f63e2c87f
USER <USER@USER-PC>
2013-04-22 00:50:46 -0300

    TRABALHO 8

--- entregas/trabalho8/src/main/java/br/com/senacrs/alp/aulas/MeuDepartamento.java
+++ entregas/trabalho8/src/main/java/br/com/senacrs/alp/aulas/MeuDepartamento.java
@@ -1,43 +1,70 @@
 package br.com.senacrs.alp.aulas;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class MeuDepartamento implements Departamento {
-
+	
+	ArrayList<Funcionario> lista = new ArrayList<Funcionario>();
+		
+	private Empresa empresa;
+	private String nome;
+	
+	public MeuDepartamento(Empresa empresa, String nome){
+		
+		if(nome == null){
+			throw new IllegalArgumentException();
+		}
+		if(empresa == null){
+			throw new IllegalArgumentException();
+		}
+		this.nome = nome;
+		this.empresa = empresa;
+	}
+	
 	@Override
 	public Empresa getEmpresa() {
 		
-		return null;
+		return this.empresa;
 	}
 
 	@Override
 	public String getNome() {
 		
-		return null;
+		return this.nome;
 	}
 
 	@Override
 	public List<Funcionario> listarFuncionariosPorOrdemAlfabetica() {
 		
-		return null;
+		ComparadorFuncionariosPorOrdemAlfabetica cfa = new ComparadorFuncionariosPorOrdemAlfabetica();
+		Collections.sort(lista, cfa);
+		return lista;
 	}
 
 	@Override
 	public List<Funcionario> listarFuncionariosPorDecrescenteSalario() {
 		
-		return null;
+		ComparadorFuncionariosPorDecrescenteSalario cds = new ComparadorFuncionariosPorDecrescenteSalario();
+		Collections.sort(lista, cds);
+		return lista;
 	}
 
 	@Override
 	public int quantidadeFuncionarios() {
 		
-		return 0;
+		return lista.size();
 	}
 
 	@Override
 	public void adicionarFuncionario(Funcionario funcionario) {
 		
-		
+		if(funcionario == null){
+			throw new IllegalArgumentException();
+		}
+		lista.add(funcionario);
 	}
 
 }
--- entregas/trabalho8/src/main/java/br/com/senacrs/alp/aulas/MinhaEmpresa.java
+++ entregas/trabalho8/src/main/java/br/com/senacrs/alp/aulas/MinhaEmpresa.java
@@ -1,31 +1,58 @@
 package br.com.senacrs.alp.aulas;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
-public class MinhaEmpresa implements Empresa{
-
+public class MinhaEmpresa implements Empresa {
+	
+	ArrayList<Departamento> lista = new ArrayList<Departamento>();
+	
+	private String nome;
+	
+	public MinhaEmpresa(String nome){
+		
+		if(nome == null){
+			throw new IllegalArgumentException();
+		}
+		this.nome = nome;
+	}
+	
 	@Override
 	public String getNome() {
-		
-		return null;
+		return this.nome;
 	}
 
 	@Override
 	public List<Departamento> listaDepartamentoOrdemCrescentePorQtdFuncionarios() {
 		
-		return null;
+		ComparadorDepartamentoOrdemCrescentePorQtdFuncionarios ccf = new ComparadorDepartamentoOrdemCrescentePorQtdFuncionarios();
+		Collections.sort(lista, ccf);
+		return lista;
 	}
 
 	@Override
 	public void adicionarDepartamento(Departamento departamento) {
 		
-		
+		if(departamento == null){
+			throw new IllegalArgumentException();
+		}
+		lista.add(departamento);
+				
 	}
 
 	@Override
-	public int quantidadeFuncionarios() {
+	public int quantidadeFuncionarios() {		
 		
-		return 0;
+		int i = 0;
+		int soma = 0;
+		while(i < lista.size()) {
+			soma = soma + lista.get(i).quantidadeFuncionarios();
+			i++;
+		}
+		
+		return soma;
+			
 	}
 
 }
