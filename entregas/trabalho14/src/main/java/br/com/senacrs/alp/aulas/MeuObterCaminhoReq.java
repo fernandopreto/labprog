508e15b7e3f5bbf9054e0a7090c937d746b28419
631220003 <631220003@Sala603-09.fspoaeduc.com.br>
2013-06-13 19:31:23 -0300

    TRABALHO 14

---
+++ entregas/trabalho14/src/main/java/br/com/senacrs/alp/aulas/MeuObterCaminhoReq.java
@@ -0,0 +1,13 @@
+package br.com.senacrs.alp.aulas;
+
+import java.io.Reader;
+
+public class MeuObterCaminhoReq implements ObterPathRequisicaoGet{
+
+	@Override
+	public String caminhoAbsoluto(Reader requisicao) {
+		
+		return null;
+	}
+
+}
