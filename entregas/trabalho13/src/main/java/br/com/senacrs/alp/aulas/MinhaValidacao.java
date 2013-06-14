46504e3f7f9e7311f5b5768a9b30f38aaf69b741
631220003 <631220003@Sala603-09.fspoaeduc.com.br>
2013-06-13 21:10:17 -0300

    TRABALHO 13

--- entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/MinhaValidacao.java
+++ entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/MinhaValidacao.java
@@ -1,5 +1,8 @@
 package br.com.senacrs.alp.aulas;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 
 public class MinhaValidacao implements ValidarRequisicaoGet{
@@ -7,7 +10,28 @@ public class MinhaValidacao implements ValidarRequisicaoGet{
 	@Override
 	public boolean ehRequisicaoGetValida(Reader requisicao) {
 		
-		return false;
+		String[] linha = new String[2];
+		
+		boolean resultado = false;
+		
+		if(requisicao == null){
+			throw new IllegalArgumentException();
+		}
+		
+		BufferedReader lerlinha = new BufferedReader(requisicao);
+		
+		try {
+			linha[0] = lerlinha.readLine();
+		} catch (IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+		try {
+			linha[1] = lerlinha.readLine();
+		} catch (IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+		
+		return resultado;
 	}
 
 }
